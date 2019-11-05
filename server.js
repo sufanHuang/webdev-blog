20a9b8e16d4e3ed766dd79bcdc6a9bbc9f3239e6
sufanHuang <sufanhk@gmail.com>
2019-11-04 23:26:08 -0600

    Adding pages

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const port = process.env.PORT || 3000
 app.use(express.static('public'))
+
 app.get('/', (req,res)=>{
  res.sendFile(__dirname + '/public/index.html')
 })
 
 
-
-app.listen(process.env.PORT,()=>{
-    console.log('server is running on port 3000')
+app.listen(port,()=>{
+    console.log(`server running on ${port}`)
 })
